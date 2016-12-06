72269919c8aef2df5d2f59c4d58ea62d8b3a6910
master.wasp <master@wasp.wasp>
2016-12-06 13:25:30 +0100

    lab network

--- longpress_0.sh
+++ longpress_0.sh
@@ -1,6 +1,6 @@
 mount -o remount,rw / &&
-wpa_passphrase 'Networkname' 'password' > /etc/wpa_supplicant.conf &&  
-ifconfig eth0 192.168.1.102 && 
+wpa_passphrase 'WASP1-24' 'wasp2016' > /etc/wpa_supplicant.conf &&  
+ifconfig eth0 10.42.0.102 && 
 wpa_supplicant -B -D wext -i eth0 -c /etc/wpa_supplicant.conf
 #wait 5 &&
 #/sbin/udhcpc -i eth0 
